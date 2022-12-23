9e90ac884a145e837ead168293ba0f1b80cabff9
Алексей <lebedevalexeyo@gmail.com>
2022-12-24 03:19:10 +1100

    Добавьте файлы проекта.

---
+++ WeatherControl.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Lab6WpfApp
+{
+    class WeatherControl : DependencyObject
+    {
+        public static readonly DependencyProperty TemperatureProperty;
+        public int Temperature
+        {
+            get => (int)GetValue(TemperatureProperty);
+            set => SetValue(TemperatureProperty, value);
+        }
+        public string WindDirection { get; set; }
+        public int WindSpeed { get; set; }
+        public enum Precipitation
+        {
+            sunny = 0,
+            cloudy = 1,
+            rainy = 2,
+            snowy = 3
+        }
+        static WeatherControl()
+        {
+            TemperatureProperty = DependencyProperty.Register(
+                nameof(Temperature),
+                typeof(int),
+                typeof(WeatherControl),
+                new FrameworkPropertyMetadata(
+                    0),
+                new ValidateValueCallback(ValidateTemperature));
+        }
+
+        private static bool ValidateTemperature(object value)
+        {
+            int v = (int)value;
+            if (v >= -50 && v <= 50)
+                return true;
+            else
+                return false;
+        }
+    }
+}
